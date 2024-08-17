55309f06967bb78eb42756bff3a62cfb445030b0
ahmmeedssherif <ahmedsheriffouad9@gmail.com>
2024-08-17 07:48:48 +0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
+import Resume from "./Components/Resume";
+
+export default function Home() {
+  return (
+    <>
+      <About/>
+      <Resume/>
+      <Portfolio/>
+      <Contact/>
+    </>
+  );
+}
